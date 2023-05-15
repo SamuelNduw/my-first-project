4dc9734f0b582718b746fcb188e353785f423a76
SamuelNduw <samuelwamankalnduw@gmail.com>
2023-05-15 22:22:39 +0200

    form validation

--- about.js
+++ about.js
@@ -63,3 +63,96 @@ function myFunction(){
       x.type = "password";
     }
 }
+
+// Form Validation
+const form = document.querySelector('#contactForm');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+const submitButton = document.querySelector('#submit')
+const body = document.querySelector('body');
+
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+
+  // Check for Empty input fields
+  if(nameInput.value === ''){
+    invalidColor(nameInput)
+  }
+  if(emailInput.value === ''){
+    invalidColor(emailInput)
+  }
+  if(messageInput.value === ''){
+    invalidColor(messageInput)
+  }
+
+  // Improve this part of the code!!!
+
+  // if(!validateEmail(event.target.value)){
+  //   window.alert('Invalid Email address!');
+  //   invalidColor(emailInput);
+  // }
+
+  // Error message
+  errorMessage()
+
+  emailInput.addEventListener('blur', function(event) {
+    if(!validateEmail(event.target.value)){
+      window.alert('Invalid Email address!');
+    }
+  })
+
+})
+
+document.addEventListener('click', function(event) {
+  if (!form.contains(event.target)) {
+    if(nameInput.style.border === '2px solid red' || emailInput.style.border == '2px solid red' || messageInput.style.border == '2px solid red'){
+      validColor(nameInput);
+      validColor(emailInput);
+      validColor(messageInput);
+    }
+    
+  }
+});
+
+function invalidColor(inputType){
+  inputType.style.border = '2px solid red';
+}
+
+function validColor(inputType){
+  inputType.style.border = '2px solid #ccc'
+}
+
+function errorMessage(){
+  let nameValue = nameInput.value;
+  let emailValue = emailInput.value;
+  let messageValue = messageInput.value;
+
+  if(nameValue === '' && emailValue === '' && messageValue === ''){
+    window.alert('Please fill in the name, email and message fields!')
+  }
+  else if(nameValue === '' && emailValue === ''){
+    window.alert('Please fill in the name and email fields!')
+  }
+  else if(nameValue === '' && messageValue === ''){
+    window.alert('Please fill in the name and message fields!')
+  }
+  else if(emailValue === '' && messageValue === ''){
+    window.alert('Please fill in the email and message fields!')
+  }
+  else if(nameValue === ''){
+    window.alert('Please fill in the name field!')
+  }
+  else if(emailValue === ''){
+    window.alert('Please fill in the email field!')
+  }
+  else if(messageValue === ''){
+    window.alert('Please fill in the message field!')
+  }
+}
+
+function validateEmail(email){
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
